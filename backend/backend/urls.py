d70591381511bd45ddc6d3b3ae3b4efea7c844f1
ALTUM-02 <martin249jesus@gmail.com>
2026-07-07 20:16:41 +0300

    new change

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+from graphene_django.views import GraphQLView
+from django  
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(schema=schema)))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

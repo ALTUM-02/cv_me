ec1a1cd4b9a876a759caa363b74b6c490c89dfdd
ALTUM-02 <martin249jesus@gmail.com>
2026-07-07 19:11:37 +0300

    new change

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 
+from datetime import timedelta
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-)y7bw0ihzq*1-fs&982#!xj28yd2bl8&-b$r_xy0&#e9-7zg@e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

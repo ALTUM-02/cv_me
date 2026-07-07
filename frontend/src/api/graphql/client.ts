10a5a3d113f2e42cf646f7aa44be3ad3908c93d1
ALTUM-02 <martin249jesus@gmail.com>
2026-07-07 21:41:13 +0300

    new change

---
+++ frontend/src/api/graphql/client.ts
@@ -0,0 +1,44 @@
+import { ApolloClient, InMemoryCache, createHttpLink, ApolloLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+  credentials: 'same-origin',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('auth_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `JWT ${token}` : '',
+    },
+  };
+});
+
+export const apolloClient = new ApolloClient({
+  link: ApolloLink.from([authLink, httpLink]),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          allCvs: {
+            merge(existing = [], incoming: unknown[]) {
+              return incoming;
+            },
+          },
+          allUsers: {
+            merge(existing = [], incoming: unknown[]) {
+              return incoming;
+            },
+          },
+        },
+      },
+    },
+  }),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'cache-and-network',
+    },
+  },
+});
